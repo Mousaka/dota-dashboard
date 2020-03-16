2444873d1f780082ad3495f3f892c7d89d0ed8cc
Kristian Lundström <kristian.lundstrom@insurello.se>
2020-03-16 18:18:44 +0100

    first

---
+++ src/Main.elm
@@ -0,0 +1,57 @@
+module Main exposing (..)
+
+import Browser
+import Element
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (src)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    Element.layout [] <|
+        Element.row []
+            [ Element.text "We!" ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
