516363503f336f7f2f9c92a9d4971c6b8b1b2ee4
Kristian Lundström <kristian.lundstrom@insurello.se>
2020-03-17 02:08:00 +0100

    kind of works

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,13 @@ module Main exposing (..)
 
 import Browser
 import Element
+import Element.Border
+import Element.Font
+import Element.Input
 import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Http
+import Json.Decode as JD
+import Json.Encode as JE
 
 
 
@@ -11,12 +16,16 @@ import Html.Attributes exposing (src)
 
 
 type alias Model =
-    {}
+    { usernameInputField : String
+    , steamIdInputField : String
+    , apiError : Maybe String
+    , users : List UserStats
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { usernameInputField = "", steamIdInputField = "", apiError = Nothing, users = [] }, getWinLoseLastWeek )
 
 
 
@@ -24,23 +33,135 @@ init =
 
 
 type Msg
-    = NoOp
+    = UsernameChanged String
+    | SteamIdChanged String
+    | AddButtonClicked
+    | AddUserResponse (Result Http.Error ())
+    | WinLoseLastWeekResponse (Result Http.Error (List UserStats))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UsernameChanged input ->
+            ( { model | usernameInputField = input }, Cmd.none )
+
+        SteamIdChanged input ->
+            ( { model | steamIdInputField = input }, Cmd.none )
+
+        AddButtonClicked ->
+            ( model, addUser model )
+
+        AddUserResponse result ->
+            case result of
+                Ok _ ->
+                    ( { model | apiError = Nothing }, Cmd.none )
+
+                Err _ ->
+                    ( { model | apiError = Just "Error with connection to api" }, Cmd.none )
+
+        WinLoseLastWeekResponse result ->
+            case result of
+                Ok users ->
+                    ( { model | apiError = Nothing, users = users }, Cmd.none )
+
+                Err error ->
+                    ( { model | apiError = Just "Error with connection to api" }, Cmd.none )
+
+
+type alias UserStats =
+    { username : String
+    , steamId : String
+    , win : Int
+    , lose : Int
+    }
+
+
+userStatsDecoder : JD.Decoder UserStats
+userStatsDecoder =
+    JD.map4 UserStats
+        (JD.field "username" JD.string)
+        (JD.field "steamId" JD.string)
+        (JD.field "win" JD.int)
+        (JD.field "lose" JD.int)
+
+
+getWinLoseLastWeek : Cmd Msg
+getWinLoseLastWeek =
+    Http.get
+        { url = "https://mousaka-dota-dashboad.builtwithdark.com/api/users"
+        , expect = Http.expectJson WinLoseLastWeekResponse (JD.list userStatsDecoder)
+        }
+
+
+addUser : Model -> Cmd Msg
+addUser model =
+    Http.post
+        { url = "https://mousaka-dota-dashboad.builtwithdark.com/api/add-user"
+        , body =
+            Http.jsonBody
+                (JE.object
+                    [ ( "username", JE.string model.usernameInputField )
+                    , ( "steamId", JE.string model.steamIdInputField )
+                    ]
+                )
+        , expect = Http.expectWhatever AddUserResponse
+        }
 
 
 
 ---- VIEW ----
 
 
+addMeView : Model -> Element.Element Msg
+addMeView model =
+    Element.column [ Element.alignBottom, Element.padding 10, Element.spacing 10 ]
+        [ Element.el []
+            (Element.text "Add me to the list")
+        , Element.Input.text
+            []
+            { onChange = UsernameChanged
+            , text = model.usernameInputField
+            , placeholder = Nothing
+            , label = Element.Input.labelAbove [] (Element.el [ Element.Font.size 14 ] (Element.text "Username"))
+            }
+        , Element.Input.text []
+            { onChange = SteamIdChanged
+            , text = model.steamIdInputField
+            , placeholder = Nothing
+            , label = Element.Input.labelAbove [] (Element.el [ Element.Font.size 14 ] (Element.text "SteamId"))
+            }
+        , Element.Input.button [ Element.Border.rounded 3, Element.Border.width 1 ]
+            { onPress = Just AddButtonClicked
+            , label = Element.el [ Element.padding 5 ] (Element.text "Add me")
+            }
+        ]
+
+
+viewUsers : List UserStats -> Element.Element Msg
+viewUsers users =
+    Element.column [ Element.paddingXY 0 10, Element.height Element.fill ]
+        (List.map
+            (\u ->
+                Element.row [ Element.spacing 10 ]
+                    [ Element.text u.username
+                    , Element.text (String.fromInt u.win ++ " wins")
+                    , Element.text (String.fromInt u.lose ++ " losses")
+                    ]
+            )
+            users
+        )
+
+
 view : Model -> Html Msg
 view model =
-    Element.layout [] <|
-        Element.row []
-            [ Element.text "We!" ]
+    Element.layout [ Element.padding 50 ] <|
+        Element.column [ Element.width Element.fill, Element.height Element.fill ]
+            [ Element.row []
+                [ Element.el [ Element.Font.bold ] (Element.text "Last 7 days of dota ") ]
+            , viewUsers model.users
+            , addMeView model
+            ]
 
 
 
